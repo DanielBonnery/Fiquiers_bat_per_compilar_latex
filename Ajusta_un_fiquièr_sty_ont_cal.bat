7a2ab010fe036f60ff0a17555da17c254c781438
Daniel Bonnéry <daniel.bonnery@gmail.com>
2018-11-01 17:32:06 -0400

    Update Ajusta_un_fiquièr_sty_ont_cal.bat

--- Ajusta_un_fiquièr_sty_ont_cal.bat
+++ Ajusta_un_fiquièr_sty_ont_cal.bat
@@ -2,4 +2,4 @@ for /f %%i in ('kpsewhich -var-value=TEXMFHOME') do set Var=%%i
 if not exist %Var%/tex mkdir %Var%/tex
 if not exist %Var%/tex/latex mkdir %Var%/tex/latex
 if not exist %Var%/tex/latex/mystylefiles mkdir %Var%/tex/latex/mystylefiles
-cp ./nielsenresearchreport2.sty %Var%/tex/latex/mystylefiles/
+cp ./examplestyfile.sty %Var%/tex/latex/mystylefiles/
